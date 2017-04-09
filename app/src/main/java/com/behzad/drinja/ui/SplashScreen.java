f6d585e250da347622e477cf8b5bba37c902bd2b
behzad nm <behzadnm99@gmail.com>
2017-04-09 13:34:45 +0430

    Initial commit

---
+++ app/src/main/java/com/behzad/drinja/ui/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.behzad.drinja.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by behzad on 4/9/17.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
